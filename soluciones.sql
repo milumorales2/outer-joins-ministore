6d714b3d370ba6928256069d4bf644dba1d642b4
milumorales2 <milumorales2@gmail.com>
2026-07-29 21:41:11 -0300

    Update soluciones.sql

--- soluciones.sql
+++ soluciones.sql
@@ -1,23 +1,61 @@
 -- ══════════════════════════════════════════
 -- MiniStore — Soluciones con Outer JOINs
 -- Autor: Milagros Lucía Morales
--- Fecha: 29-07-2026
+-- Fecha: 30-07-2026
 -- ══════════════════════════════════════════
+
+USE MINISTORE;
+
 -- ── CONSULTA 1: LEFT JOIN ─────────────────
 -- Pregunta de negocio: ¿Qué productos del catálogo nunca fueron vendidos?
 -- Mostrá todos los productos y sus ventas asociadas.
 -- Los productos sin ventas aparecerán con NULL en las columnas de ventas.
 
 SELECT 
-
+	p.producto_id,
+	p.nombre AS producto,
+	p.categoria,
+    p.precio,
+    v.venta_id,
+    v.cliente_id,
+    v.cantidad,
+    v.fecha_venta
 FROM productos AS p
+LEFT JOIN ventas AS v
+ON p.producto_id = v.producto_id;
+
 -- ── CONSULTA 2: RIGHT JOIN ────────────────
 -- Pregunta de negocio: ¿Existen ventas registradas con productos
 -- que no figuran en nuestro catálogo? (posible error de carga de datos)
 -- Los registros huérfanos aparecerán con NULL en las columnas de productos.
--- [Tu consulta aquí]
+
+SELECT 
+	p.producto_id,
+	p.nombre AS producto,
+	p.categoria,
+    p.precio,
+    v.venta_id,
+    v.cliente_id,
+    v.cantidad,
+    v.fecha_venta
+FROM productos AS p
+RIGHT JOIN ventas AS v
+ON p.producto_id = v.producto_id;
+
 -- ── CONSULTA 3: FULL OUTER JOIN ───────────
 -- Pregunta de negocio: Vista completa de auditoría que muestre
 -- todos los productos y todas las ventas sin perder ninguna fila,
 -- identificando tanto productos sin ventas como ventas sin producto.
--- [Tu consulta aquí]
+
+SELECT 
+	p.producto_id,
+	p.nombre AS producto,
+	p.categoria,
+    p.precio,
+    v.venta_id,
+    v.cliente_id,
+    v.cantidad,
+    v.fecha_venta
+FROM productos AS p
+FULL OUTER JOIN ventas AS v
+ON p.producto_id = v.producto_id;
